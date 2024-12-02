19f861507b76a824c0e2a276f701c600ada5cffb
David <d.f.brooks01@gmail.com>
2024-12-02 14:35:33 +0100

    Renamed solution file

--- day_1/d1.py
+++ day_1/d1.py
@@ -1,4 +1,5 @@
 import time
+import timeit
 from collections import Counter
 
 t0 = time.perf_counter()
@@ -26,10 +27,23 @@ with open(f"day_1/{INPUT}.txt", "r") as f:
 list_1.sort()
 list_2.sort()
 
-diff = 0
 
+# def int_add():
+diff = 0
 for num_1, num_2 in zip(list_1, list_2):
    diff += abs(num_1 - num_2)
+      # return diff
+
+# def list_comp():
+#    return sum([abs(right - left) for left, right in zip(list_1, list_2)])
+
+# diff = int_add()
+
+# execution_time = timeit.timeit("int_add()", globals=globals(), number=10000)
+# print(f"Execution time int_add(): {execution_time:.4f} seconds for 10000 runs")
+
+# execution_time = timeit.timeit("list_comp()", globals=globals(), number=10000)
+# print(f"Execution time list_comp(): {execution_time:.4f} seconds for 10000 runs")
 
 t1 = time.perf_counter()
 print(f"Part One: {diff}, took {(t1 - t0) * 1000:.3f} ms")
@@ -37,11 +51,13 @@ print(f"Part One: {diff}, took {(t1 - t0) * 1000:.3f} ms")
 # Using a Counter object over .count() improves speed by ~20x
 my_counter = Counter(list_2)
 
+
 total = 0
 for num in list_1:
-   # Here, avoiding 0 multiplication saves ~0.4 ms, even with the additional assignment to "frequency" variable
-   frequency = my_counter[num]
-   if frequency:
-      total += (num * frequency) 
+   # Here, avoiding 0 multiplication saves ~0.4 ms
+   if my_counter[num]:
+      total += (num * my_counter[num]) 
+
+
 
 print(f"Part Two: {total}, took {(time.perf_counter() - t1) * 1000:.3f} ms")
