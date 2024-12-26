e1f91d58c245b540e6599f289af883bb9e89ce52
David <d.f.brooks01@gmail.com>
2024-12-26 13:03:33 +0100

    Part 2

--- day_13/d13.py
+++ day_13/d13.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 INPUT = "input"
 
@@ -6,40 +7,54 @@ t0 = time.perf_counter()
 
 
 def extract_coords(line):
-    field_mapping = {"A": 3, "B": 1}
-    temp_coords = [field_mapping.get(line[7], 0)]
+    temp_coords = []
     for coord in line.split(","):
         temp_coords.append(int("".join([digit for digit in coord if digit.isdigit()])))
-    return tuple(temp_coords)
+    return temp_coords
 
 
 with open(f"day_13/{INPUT}.txt") as f:
     data = []
     for machine in f.read().split("\n\n"):
-        machine_data = []
+        tmp = []
         for line in machine.split("\n"):
-            machine_data.append(extract_coords(line))
+            tmp.append(extract_coords(line))
+        machine_data = {
+            "coefficients": np.array([[tmp[0][0], tmp[1][0]], [tmp[0][1], tmp[1][1]]]),
+            "constants": np.array(tmp[2]),
+        }
         data.append(machine_data)
 
 
-def validate_combination(combination, target):
-    x = sum([press[1] for press in combination])
-    y = sum([press[2] for press in combination])
-    return (x, y) == target[1:]
+def confirm_integer(solution):
+    return all(abs(num - round(num)) < 0.0001 and round(num) >= 0 for num in solution)
 
 
-max_repeats = 100
-total_tokens = 0
-for machine in data:
-    valid = False
-    for count0 in range(max_repeats + 1):
-        for count1 in range(max_repeats + 1):
-            if not valid:
-                combination = [machine[0]] * count0 + [machine[1]] * count1
-                if validate_combination(combination, machine[2]):
-                    valid = True
-                    total_tokens += sum([press[0] for press in combination])
-                    print(f"{total_tokens=}")
+def find_solutions(data, pt1: bool = True):
+    x, y = 0, 0
+    for machine in data:
+        solution = np.linalg.solve(machine["coefficients"], machine["constants"])
+        if not confirm_integer(solution) or (pt1 and any(z > 100 for z in solution)):
+            continue
+        x += solution[0]
+        y += solution[1]
 
+    return x, y
+
+
+x, y = find_solutions(data, pt1=True)
+total_tokens = int((x * 3) + y)
 t1 = time.perf_counter()
-print(f"Part One: {total_tokens}, took {t1 - t0:.3f} s")
+print(f"Part One: {total_tokens}, took {(t1 - t0)*1000:.3f} ms")
+
+INCREASE = 10000000000000
+
+for machine in data:
+    machine["constants"][0] += INCREASE
+    machine["constants"][1] += INCREASE
+
+
+x, y = find_solutions(data, pt1=False)
+total_tokens = int((x * 3) + y)
+t2 = time.perf_counter()
+print(f"Part Two: {total_tokens}, took {(t2 - t1)*1000:.3f} ms")
